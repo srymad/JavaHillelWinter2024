a76eb45b842ddbc5446d1dabbe10b22d928bad59
srymad <zasd4478@gmail.com>
2024-04-24 14:46:15 +0300

    hw31

--- homeworks_with_maven/hw30/src/main/java/org/example/dao/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.dao;
-
-
-import org.example.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
